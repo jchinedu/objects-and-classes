5fc3f229da6a04f6697b4e0b13d3566edec9fefb
jchinedu <jchinedu080@gmail.com>
2025-06-28 21:09:18 +0100

    progress

--- src/ProblemType.java
+++
@@ -1,8 +0,0 @@
-public enum ProblemType {
-     FINANCIAL,
-    EDUCATIONAL,
-     TECHNICAL,
-     SPIRITUAL,
-     MENTAL,
-
-}
--- src/StudentGrade.java
+++
@@ -1,5 +0,0 @@
-public class StudentGrade {
-    public static void main(String[] args) {
-        System.out.print("Hello world");
-    }
-}
